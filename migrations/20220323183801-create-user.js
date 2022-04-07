abf761d47d9216c59541324ab0a4c6e83d5eaf22
enesd <enesdem175@gmail.com>
2022-04-07 17:12:47 +0300

    avatar photo to database added

---
+++ migrations/20220323183801-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_name: {
+        type: Sequelize.STRING
+      },
+      full_name: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      emailAktif :  {
+        type : Sequelize.BOOLEAN,
+        default : false
+
+      },
+      avatar : {
+        type : Sequelize.STRING,
+        default  : 'https://i.tmgrup.com.tr/gq/original/17-06/22/user_male_circle_filled1600.png',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
